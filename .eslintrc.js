35db1d26dab067760b27beedbe09f4d40e939f5b
ochi <ocos54ce@gmail.com>
2020-09-30 02:00:05 +0900

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es2021": true,
+    "node": true
+  },
+  "extends": "eslint:recommended",
+  "parserOptions": {
+    "ecmaVersion": 12,
+    "sourceType": "module"
+  },
+  "rules": {
+    "indent":[
+      "error",
+      2,
+      {"SwitchCase": 1}
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "double"
+    ],
+    "semi": [
+      "error",
+      "always"
+    ],
+    "no-unused-vars":[
+      "error",
+      {
+        "vars": "all",
+        "args": "none"
+      }
+    ],
+    "no-console":[
+      "off"
+    ]
+  }
+};
