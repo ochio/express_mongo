58904dd0a4be00fe6823462a7cf5d280b6456d1e
ochi <ocos54ce@gmail.com>
2020-10-01 00:03:30 +0900

    add application log

--- config/log4js.config.js
+++ config/log4js.config.js
@@ -11,6 +11,12 @@ module.exports = {
       filename: path.join(ROOT, "./log/system/system.log"),
       maxLogSize: 5000000,
       backup: 10
+    },
+    MultifileLogAppender: {
+      type: "multiFile",
+      base: path.join(ROOT, "./log/application/"),
+      property: "key",
+      extension: ".log"
     }
   },
   categories:{
@@ -20,6 +26,10 @@ module.exports = {
     },
     system: {
       appenders: ["FileLogAppender"],
+      level: "ERROR"
+    },
+    application: {
+      appenders: ["MultifileLogAppender"],
       level: "ERROR"
     }
   }
