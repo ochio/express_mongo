5b4d6412b222ee92bccecb1e1993c9a22f42a0da
ochi <ocos54ce@gmail.com>
2020-09-30 23:06:55 +0900

    add systemLog

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const systemlogger = require("./lib/log/systemlogger");
 const express = require("express");
 const app = express();
 
@@ -7,5 +8,7 @@ app.disable("x-powered-by");
 app.use("/public", express.static(__dirname + "/public/" + (process.env.NODE_ENV === "development" ? "development" : "production")));
 
 app.use("/", require("./routes/index"));
+
+app.use(systemlogger());
 
 app.listen(3000);
