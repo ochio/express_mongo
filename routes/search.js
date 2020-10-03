56ba541142db00c219fe4dd26d40b929599828cb
ochi <ocos54ce@gmail.com>
2020-10-03 16:54:49 +0900

    add pagination

--- routes/search.js
+++ routes/search.js
@@ -1,28 +1,46 @@
 const {CONNECTION_URL, OPTIONS, DATABASE} = require("../config/mongodb.config");
+2;
+const {MAX_ITEM_PER_PAGE} = require("../config/app.config").search;
 const router = require("express").Router();
 const MongoClient = require("mongodb").MongoClient;
 
 router.get("/*", (req, res) => {
+  const page = req.query.page ? parseInt(req.query.page) : 1;
   const keyword = req.query.keyword || "";
   const regexp = new RegExp(`.*${keyword}.*`);
+  const query = {
+    $or: [{title: regexp}, {content: regexp}]
+  };
+
   MongoClient.connect(CONNECTION_URL, OPTIONS, (error, client) => {
     const db = client.db(DATABASE);
-    db.collection("posts").find({
-      $or: [{title: regexp}, {content: regexp}]
-    }).sort({
-      published: -1
-    }).toArray()
-      .then((list) => {
-        const data = {
-          keyword,
-          list
-        };
-        res.render("./search/list.ejs",data);
-      }).catch((error) => {
-        throw error;
-      }).then(() => {
-        client.close();
-      });
+		
+    Promise.all([
+      db.collection("posts")
+        .find(query)
+        .count(),
+      db.collection("posts")
+        .find(query)
+        .sort({published: -1})
+        .skip((page - 1) * MAX_ITEM_PER_PAGE)
+        .limit(MAX_ITEM_PER_PAGE)
+        .toArray()
+    ]).then((results) => {
+      const data = {
+        keyword,
+        count: results[0],
+        list: results[1],
+        pagination: {
+          max: Math.ceil(results[0] / MAX_ITEM_PER_PAGE),
+          current: page
+        }
+      };
+      res.render("./search/list.ejs",data);
+    }).catch((error) => {
+      throw error;
+    }).then(() => {
+      client.close();
+    });
   });
 });
 
